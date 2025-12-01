d93c0fcab8dab3c9bfc1b9cf2294bd768f2bd011
mojtaban <mojtaba.n@behinrahkar.com>
2025-12-01 13:15:52 +0330

    feat: add Auction entries and DependancyInjection PostgreSQL EntityCore package

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,18 @@
+namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; }
+    public required string Seller { get; set; }
+    public string? Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; } = DateTime.UtcNow;
+    public Status Status { get; set; } = Status.Live;
+    public required Item Item { get; set; }
+
+
+}
---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,16 @@
+namespace AuctionService.Entities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public required string Make { get; set; }
+    public required string Model { get; set; }
+    public int Year { get; set; }
+    public required string Color { get; set; }
+    public int Mileage { get; set; }
+    public required string ImageUrl { get; set; }
+
+    // nav properties
+    public required Auction Auction { get; set; }
+    public Guid AuctionId { get; set; }
+}
