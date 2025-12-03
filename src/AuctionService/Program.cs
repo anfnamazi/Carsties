eaa598b7ff2b4fa2eb0023ad0d30c32aacf66eca
mojtaban <mojtaba.n@behinrahkar.com>
2025-12-03 15:47:44 +0330

    feat: add DTOs class & Mapping profile auto mapper service

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService.Data;
+using AuctionService.RequestHelpers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
