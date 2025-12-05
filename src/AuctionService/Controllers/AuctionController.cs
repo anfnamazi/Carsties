8503872316a202fa0e6497fac5f74295c98f84b5
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-12-05 15:37:01 +0330

    feat: add update auction api

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -54,4 +54,25 @@ public class AuctionController : ControllerBase
         return CreatedAtAction(nameof(GetAllAuction),
             new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuction updateAuctionDto)
+    {
+        var auction = await _context.Auctions.Include(a => a.Item)
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null) return NotFound();
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Problem in saving changes");
+
+        return Ok();
+    }
 }
