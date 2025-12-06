d3ca8e81b3e53c566650e9a737a573033b60c41e
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-12-07 00:17:32 +0330

    feat: add mongo db connection config

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -9,5 +13,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDB", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+    .Key(i => i.Make, KeyType.Text)
+    .Key(i => i.Model, KeyType.Text)
+    .Key(i => i.Color, KeyType.Text)
+    .CreateAsync();
+
 
 app.Run();
