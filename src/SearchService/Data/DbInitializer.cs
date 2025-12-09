e37dc38e834d2820da328b799fcd0194ebe15e84
mojtaban <mojtaba.n@behinrahkar.com>
2025-12-09 17:27:45 +0330

    feat: In AuctionService add synchronous http client to fetch data from AuctionService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -21,15 +22,14 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed data");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        using var scoped = app.Services.CreateScope();
 
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        var httpClient = scoped.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
 
-            await DB.SaveAsync(items);
-        }
+        Console.WriteLine(items.Count + "items fetched from auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+
     }
 }
