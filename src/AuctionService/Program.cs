af8577ca848f43506382efe42dda9a34a9a88512
mojtaban <mojtaba.n@behinrahkar.com>
2025-12-15 13:22:13 +0330

    feat: add auction-created fault consumer to the auction service to handle ArgumentException from the search service consumer

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
 using AuctionService.RequestHelpers;
 using MassTransit;
@@ -22,6 +23,9 @@ builder.Services.AddMassTransit(x =>
         o.UsePostgres();
         o.UseBusOutbox();
     });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
